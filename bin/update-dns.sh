6a51ec0ded14b457b7100194e23c6cf0b876525c
Dirk Petersen <peterdir@ohsu.edu>
2023-05-16 12:02:35 -0700

    have update-dns.sh use dns servers from resolv.conf

--- bin/update-dns.sh
+++ bin/update-dns.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Get hostname
@@ -7,8 +6,19 @@ HOSTNAME=$(hostname)
 # Get IP
 IP=$(hostname -I | awk '{print $1}')
 
+# Get DNS servers from /etc/resolv.conf
+DNS_SERVERS=$(awk '/^nameserver/ {print $2}' /etc/resolv.conf)
+
 # Send information to DNS server
-echo "sending ${HOSTNAME} = ${IP}"
+echo "sending ${HOSTNAME} = ${IP} to localhost"
 curl -X POST -d "hostname=$HOSTNAME&ip=$IP" http://localhost:5000/update_record
+
+# Send information to each DNS server
+for DNS_SERVER in $DNS_SERVERS; do
+  echo "sending ${HOSTNAME} = ${IP} to ${DNS_SERVER}"
+  #curl -X POST -d "hostname=$HOSTNAME&ip=$IP" http://$DNS_SERVER:5000/update_record
+done
+
+
 
 
